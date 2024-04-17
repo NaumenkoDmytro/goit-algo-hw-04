25efce0ea0dda7f389740afbfdd1017fb37c0587
NaumenkoDmytro <naymenko.dmitry5@gmail.com>
2024-04-17 17:21:56 +0300

    the task is done but the code is bullshit

--- Task_1.py
+++ Task_1.py
@@ -10,3 +10,26 @@ Sitarama Raju,1000
 
 Ваше завдання - розробити функцію total_salary(path), яка аналізує цей файл і повертає загальну та середню суму заробітної плати всіх розробників.
 '''
+import re
+
+def total_salary(path):
+    with open(path, 'r', encoding='utf-8') as fh:
+        salary_list = list()
+        counter = 0
+        for line in fh:
+            counter += 1
+            salary = line.strip().split(',')
+            salary = ' '.join(salary)
+            salary_num = int(re.sub('\D','',salary))
+            salary_list.append(salary_num)
+        total_salary=sum(salary_list)
+        print(total_salary)
+        print(counter)
+        average_salary = total_salary / counter
+
+    return total_salary, average_salary
+
+
+
+total, average = total_salary('salary_file.txt')
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
