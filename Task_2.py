1288107881739b3ea6a3a9c67e4ca6d8c424af3f
NaumenkoDmytro <naymenko.dmitry5@gmail.com>
2024-04-18 11:39:21 +0300

    Second Task added

--- Task_2.py
+++ Task_2.py
@@ -11,3 +11,46 @@
 Ваше завдання - розробити функцію get_cats_info(path), яка читає цей файл та повертає список словників з інформацією про кожного кота.
 
 '''
+
+import re
+
+def get_cats_info(path):
+    final_list = list()
+    with open(path, 'r', encoding='utf-8') as fh:
+        for line in fh:
+                try:
+                    split_the_line = line.strip().split(',')
+                    if len(split_the_line) == 3: #without this check I had AttributeError: 'list' object attribute 'append' is read-only how to fix it
+                        cat_info = {
+                            'id': split_the_line[0],
+                            'name': split_the_line[1],
+                            'age': split_the_line[2]
+                        }
+                        final_list.append(cat_info)
+                except IndexError:
+                    print(f"Error: Line '{line.strip()}' does not contain all necessary elements.")
+                except FileNotFoundError:
+                    print(f"Error: File '{path}' not found.")
+                except Exception as e:
+                    print(f"Error: {e}")
+    return final_list
+
+
+cats_info = get_cats_info("cats_file.txt")
+print(cats_info)
+
+#discuss with mentor
+
+# def get_cats_info(path):
+#     with open(path, 'r', encoding='utf-8') as fh:
+#         final_list = list()
+#         for line in fh:
+#             split_the_line = line.strip().split(',')
+#             print(type(split_the_line))
+#             for item in split_the_line:
+#                 final_list.append =({
+#                     'id' : item[0],
+#                     'name': item[1],
+#                     "age": item[2]
+#                 })
+# here is my code but it has AttributeError: 'list' object attribute 'append' is read-only how to fix it
