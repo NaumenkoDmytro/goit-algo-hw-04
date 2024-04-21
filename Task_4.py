5aeb925c9e156d957d405276d0d9cfcfb3e51c72
NaumenkoDmytro <naymenko.dmitry5@gmail.com>
2024-04-21 19:55:57 +0300

    Task 4

--- Task_4.py
+++ Task_4.py
@@ -10,3 +10,64 @@
 На першому етапі наш бот-асистент повинен вміти зберігати ім'я та номер телефону, знаходити номер телефону за ім'ям, змінювати записаний номер телефону, виводити в консоль всі записи, які зберіг. 
 Щоб реалізувати таку нескладну логіку, скористаємося словником. У словнику будемо зберігати ім'я користувача, як ключ, і номер телефону як значення.
 '''
+
+def show_all(contacts):
+    return contacts
+
+def show_phone(args, contacts):
+    if args:
+        name = args[0]
+        if name in contacts:
+            return contacts[name]
+        else:
+            return f'User name is empty' 
+    else:
+        return f'User name was not provided' 
+
+      
+def change_contact(args, contacts):
+    if args < 2:
+        return f'Enter the some more data'
+    name, phone = args
+    if name in contacts:
+        contacts[name] = phone
+        return f'The contact  for {name}, was updated :)'
+
+
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, *args
+
+def add_contact(args, contacts):
+    if args < 2:
+        return f'Enter the some more data'
+    name, phone = args
+    contacts[name] = phone
+    return "Contact added."
+
+def main():
+    contacts = {}
+    print("Welcome to the assistant bot!")
+    while True:
+        user_input = input("Enter a command: ")
+        command, *args = parse_input(user_input)
+
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+        elif command == "hello":
+            print("How can I help you?")
+        elif command == "add":
+            print(add_contact(args, contacts))
+        elif command == "change":
+            print(change_contact(args, contacts))
+        elif command == "phone":
+            print(show_phone(args, contacts))
+        elif command == "all":
+            print(show_all(contacts))
+        else:
+            print("Invalid command.")
+
+if __name__ == "__main__":
+    main()
